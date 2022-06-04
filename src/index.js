fac6fde171b0baf0220bad228dc8d848cbfd8c65
Aung <akhantz250@gmail.com>
2022-06-04 22:14:29 +0800

    improved add task feature

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import displayController from './modules/display'
 // const today =  format(new Date(2014, 1, 11), 'MM/dd/yyyy');
 // console.log(today);
 
-displayController.setUpTaskForm();
+displayController.setUpAddTask();
--- src/modules/display.js
+++ src/modules/display.js
@@ -1,13 +1,33 @@
 import {taskController} from './taskController';
 const displayController = (function(){
-
     const initialBox = 'default';
     let currentBox = initialBox;
     
-
     function setUpTaskForm(){
         const formContainerElement = document.querySelector('#form-container');
+        formContainerElement.innerHTML = `
+        <div class="task-form">
+                <form id="new-task" action="#" method="post" autocomplete="off">
+                    <div class="form-row"><input type="text" id="title" name="title" placeholder="Task name"></div>
+                    <div class="form-row"><textarea name="description" id="description" placeholder="Description" rows="5"></textarea></div>
+                    <div class="form-row" id="final-row">
+                        <label for="due-date">Due Date: <input type="date" name="due-date" id="due-date"></label>
+                        <label for="priority">Priority: 
+                            <select name="priority" id="priority">
+                                <option value="0" selected> - </option>
+                                <option value="1">Low</option>
+                                <option value="2">Medium</option>
+                                <option value="3">High</option>
+                            </select>
+                        </label>
+                        <div class="btn-container"><button id="cancel" type="button">Cancel</button>
+                            <button id="create-task" type="submit" disabled>Add task</button></div>
+                    </div>
+                </form>
+            </div>
+        `
         const addTaskBtn = document.querySelector('#create-task');
+        const cancelBtn = document.querySelector('#cancel');
         const taskNameInput = document.querySelector('#title');
         const formElement = document.querySelector('#new-task')
         taskNameInput.addEventListener('input', (e) =>{
@@ -21,6 +41,7 @@ const displayController = (function(){
         });
 
         const form = document.querySelector('#new-task')
+        cancelBtn.addEventListener('click',() => setUpAddTask());
         addTaskBtn.addEventListener('click',(e) => {
             e.preventDefault();
             const title = form.elements['title'].value;
@@ -31,12 +52,23 @@ const displayController = (function(){
             taskController.createTask(title, description, dueDate, priority, currentBox);
             _createNewTask(title, description, dueDate, priority);
             console.log(taskController.allTasks);
-
+            setUpAddTask();
             form.reset();
         });
     }
 
-    function hideTaskForm(){
+    function setUpAddTask(){
+        const formContainerElement = document.querySelector('#form-container');
+        formContainerElement.innerHTML =`
+        <div id="add-task">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M11 19V13H5V11H11V5H13V11H19V13H13V19Z"/></svg>
+                    <p>Add task</p>
+                </div>
+        `;
+        const addTaskBtn = document.querySelector('#add-task');
+        addTaskBtn.addEventListener('click', () => {
+            setUpTaskForm();
+        })
          
     }
 
@@ -93,7 +125,7 @@ const displayController = (function(){
         newInfoContainer.appendChild(newDescription);
     }
     
-    return {setUpTaskForm}
+    return {setUpTaskForm,setUpAddTask}
 })();
 
 export default displayController;
